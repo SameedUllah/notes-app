4c7d8c291753619209d179a3115ed7a4649fa88f
sameedullah <sameedu786@gmail.com>
2022-10-09 16:28:26 +0500

    feat(server): bootstrap node server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,29 @@
-import express from 'express';
+import express from "express";
+// import path from "path";
+import fs from "fs";
+const cors = require("cors");
+
+const path = require("path");
 
 
-const app = express()
-const port = 4000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const router = express.Router();
+const port = 4000;
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use(cors());
+
+
+app.get("/home", (req, res) => {
+  // res.sendFile(path.join(__dirname, "public", "index.html"));
+
+  // res.render("index");
+  // res.send('hello')
+  res.sendFile(path.resolve(".././client/public/index.html"));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
